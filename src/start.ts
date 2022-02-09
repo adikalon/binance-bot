3d380998b4f983f15b0149f86faf611ca0d955cc
adikalon <webadev@yandex.ru>
2022-02-10 01:41:56 +0300

    Опечатка

--- src/start.ts
+++ src/start.ts
@@ -139,7 +139,7 @@ const log = new OneLog(logger);
       const profitSum = quantityBid * priceBid;
 
       if (profitSum < symbolConfig.minBuy) {
-        throw new Error(`Нельзя продать (${profitSum}) на сумму меньше положеной (${symbolConfig.minBuy})`);
+        throw new Error(`Нельзя продать (${profitSum}) на сумму меньше положенной (${symbolConfig.minBuy})`);
       }
 
       let orderBidQty = quantityBid.toFixed(symbolConfig.fixedCoin);
